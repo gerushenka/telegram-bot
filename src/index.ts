11dfd64a7c518aaf3683509f7d5fc9312f056ec1
Chamber6821 <vania.osinin@yandex.ru>
2024-10-25 20:41:42 +0300

    Implement hello world on express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import express from 'express'
+
+const app = express()
+
+app.get('/', async (req, res, next) => {
+  const name = req.query['name']
+  res.send(name ? `Hello Mr. ${name}` : 'Hello World!')
+  next()
+})
+
+app.listen(8080)
