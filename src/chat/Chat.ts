78e340ec1b9495fa7137bb07dfc752eda670e262
german <112763670+gerushenka@users.noreply.github.com>
2024-11-01 01:03:58 +0300

    delete comment

--- src/chat/Chat.ts
+++ src/chat/Chat.ts
@@ -2,6 +2,6 @@ import File from "../file/File";
 
 export default interface Chat {
   sendText(message: string): Promise<void>;
-  sendImage(image: File): Promise<void>; // Изменили тип на File
+  sendImage(image: File): Promise<void>;
   sendFile(file: File): Promise<void>;
 }
