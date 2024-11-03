e2a592da17a8238a958eb84c892df7d6145a8142
german <112763670+gerushenka@users.noreply.github.com>
2024-11-03 21:56:07 +0300

    fix

--- src/price/PriceWithDiscount.ts
+++ src/price/PriceWithDiscount.ts
@@ -5,14 +5,15 @@ export class PriceWithDiscount implements Price {
   private readonly discountRate: number;
 
   constructor(basePrice: Price, discountRate: number = 0) {
+    if (discountRate < 0 || discountRate > 100) {
+      throw new Error("Discount rate must be between 0 and 100.");
+    }
     this.basePrice = basePrice;
     this.discountRate = discountRate;
   }
 
   async amount(): Promise<number> {
-    if (this.discountRate < 0 && this.discountRate > 100) {
-      throw new Error("Discount rate must be between 0 and 100.");
-    }
+
     const originalAmount = await this.basePrice.amount();
     return originalAmount * (1 - this.discountRate / 100);
   }
