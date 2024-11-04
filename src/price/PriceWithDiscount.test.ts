d9af3af0765881abc17887259ebf3a699367b30e
german <112763670+gerushenka@users.noreply.github.com>
2024-11-05 01:35:14 +0300

    add resolves to PriceWithDiscount.test

--- src/price/PriceWithDiscount.test.ts
+++ src/price/PriceWithDiscount.test.ts
@@ -3,25 +3,25 @@ import { NumberPrice } from './NumberPrice';
 
 describe('PriceWithDiscount', () => {
   it('must apply the discount correctly', async () => {
-    expect(await new PriceWithDiscount(new NumberPrice(100), 20).amount()).toBe(80);
+    await expect(new PriceWithDiscount(new NumberPrice(100), 20).amount()).resolves.toBe(80);
   });
 
   it('must refund the full price at 0% discount', async () => {
-    expect(await new PriceWithDiscount(new NumberPrice(100), 0).amount()).toBe(100);
+    await expect(new PriceWithDiscount(new NumberPrice(100), 0).amount()).resolves.toBe(100);
   });
 
   it('must return a zero price at a 100% discount', async () => {
-     expect(await new PriceWithDiscount(new NumberPrice(100), 100).amount()).toBe(0);
+    await expect(new PriceWithDiscount(new NumberPrice(100), 100).amount()).resolves.toBe(0);
   });
 
-  it('must throw an error if the discount is less than 0%',async () => {
-    await expect( new PriceWithDiscount(new NumberPrice(100), -10).amount()).rejects.toThrow(
+  it('must throw an error if the discount is less than 0%', async () => {
+    await expect(new PriceWithDiscount(new NumberPrice(100), -10).amount()).rejects.toThrow(
       "Discount rate must be between 0 and 100."
     );
   });
 
   it('must throw an error if the discount is greater than 100%', async () => {
-    await expect( new PriceWithDiscount(new NumberPrice(100), 110).amount()).rejects.toThrow(
+    await expect(new PriceWithDiscount(new NumberPrice(100), 110).amount()).rejects.toThrow(
       "Discount rate must be between 0 and 100."
     );
   });
