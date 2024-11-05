31f1dd7e71c53f7c18b7fca9fdb14a868db373d3
german <112763670+gerushenka@users.noreply.github.com>
2024-11-05 13:14:08 +0300

    delete prefix in FakeChat

--- src/chat/Chat.ts
+++ src/chat/Chat.ts
@@ -10,14 +10,14 @@ export class FakeChat implements Chat {
   public chatContent = '';
 
   async sendText(message: string): Promise<void> {
-    this.chatContent += `\n\nText: ${message}`;
+    this.chatContent += `\n\n${message}`;
   }
 
   async sendImage(image: File): Promise<void> {
-    this.chatContent += `\n\nImage: [file: ${(await image.url()).toString()}]`;
+    this.chatContent += `\n\n[file: ${(await image.url()).toString()}]`;
   }
 
   async sendFile(file: File): Promise<void> {
-    this.chatContent += `\n\nFile: [file: ${(await file.url()).toString()}]`;
+    this.chatContent += `\n\n[file: ${(await file.url()).toString()}]`;
   }
 }
