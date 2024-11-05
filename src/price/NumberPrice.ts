b6020ad011751ccaafc773b0bd77eac69a345950
german <112763670+gerushenka@users.noreply.github.com>
2024-11-05 20:20:23 +0300

    delete empty line

--- src/price/NumberPrice.ts
+++ src/price/NumberPrice.ts
@@ -1,6 +1,6 @@
 import Price from "./Price";
-export class NumberPrice implements Price {
 
+export class NumberPrice implements Price {
   constructor(
     private readonly price: number
   ) {}
--- src/price/PriceWithDiscount.ts
+++ src/price/PriceWithDiscount.ts
@@ -1,6 +1,6 @@
 import Price from "./Price";
-export class PriceWithDiscount implements Price {
 
+export class PriceWithDiscount implements Price {
   constructor(
     private readonly origin: Price,
     private readonly discountRate: number = 0
